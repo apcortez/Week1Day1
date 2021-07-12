4a0ffe36eb7d2eff763b5e514a9cf155f43162b5
Angelica Cortez <angelica.cortez@avanade.com>
2021-07-12 14:51:18 +0200

    commenti

--- Week1Day1/Program.cs
+++ Week1Day1/Program.cs
@@ -6,7 +6,11 @@ namespace Week1Day1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); //serve per stampare a video Hello World
+            /* questo è
+             * un commento
+             * su più righe */
+            
         }
     }
 }
